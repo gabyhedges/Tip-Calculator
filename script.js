6e9f76757e351b593a9556d4bd61352d9a2e7c03
Gaby hedges <79882381+gabyhedges@users.noreply.github.com>
2024-10-06 12:30:35 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function calculateTip() {
+
+const  billAmount = parseFloat(document.getElementById("bill-amount").value);
+
+const  tipPercentage = parseFloat(document.getElementById("tip-percentage").value);
+
+const tipAmount = billAmount * (tipPercentage/100);
+
+const totalAmount = billAmount + tipAmount;
+
+document.getElementById("tip-amount").textContent = "$" + tipAmount.toFixed(2);
+
+document.getElementById("total-amount").textContent = "$" + totalAmount.toFixed(2);
+
+
+
+}
